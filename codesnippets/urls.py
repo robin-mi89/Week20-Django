f2cd9ce2d86f83295e6f3633b1c43da155dfd3b0
Robin Mi <si.mi5689@gmail.com>
2017-07-04 14:54:26 -0700

    changed index to default url

--- codesnippets/urls.py
+++ codesnippets/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'codesnippets'
 urlpatterns = [
-    url(r'^index/$', views.CodeListView.as_view(), name='index'),
+    url(r'', views.CodeListView.as_view(), name='index'),
 
     url(r'^detail/(?P<pk>[0-9]+)/$', 
         views.CodeDetailView.as_view(), name='detail'),
